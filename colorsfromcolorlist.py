b7b57b965554f0c47632e530095d10baca581ea8
jilsinaparveenkv <146071330+jilsinaparveenkv@users.noreply.github.com>
2023-11-16 14:19:51 +0530

    Delete colorsfromcolorlist.py

--- colorsfromcolorlist.py
+++
@@ -1,4 +0,0 @@
-#print out all colors from color_list not contained in color_list 2
-color_list_1 = set(["white","black","red"])
-color_list_2 = set(["red","green"])
-print(color_list_1.difference(color_list_2))
